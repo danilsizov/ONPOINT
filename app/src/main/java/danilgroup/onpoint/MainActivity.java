f73042f5dac6c5277cef652997a91308bae8ce7b
Danil Sizov <danilsizov3@gmail.com>
2016-04-16 16:12:56 +0300

    go maps, i'm create

--- app/src/main/java/danilgroup/onpoint/MainActivity.java
+++ app/src/main/java/danilgroup/onpoint/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,6 +22,11 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     public static String LOG_TAG = "my_log";
+    public TextView myName;
+    public TextView myXP;
+    public TextView myLevelView;
+    public ProgressBar myProgressBar;
+    public int delta = 10;// Вынести куда-то
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +35,32 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();new ParseTask().execute();
     }
 
+    int level_xp (int level) {
+        return level >= 2 ? 5 * (level + 2) * (level - 1) : 0;
+    }
+
+    int get_level(int exp) {
+        int i = 2;
+        int level = 0;
+        while(exp >= 0) {
+            exp -= (i++)*delta;
+            level += 1;
+        }
+        return level;
+    }
+
     private class ParseTask extends AsyncTask<Void, Void, String> {
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String resultJson = "";
+        String in;
 
         @Override
         protected String doInBackground(Void... params) {
             // получаем данные с внешнего ресурса
             try {
-                URL url = new URL("http://androiddocs.ru/api/friends.json");
+                URL url = new URL("http://83.220.175.77:3000/user.json");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -68,33 +89,57 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(strJson);
             // выводим целиком полученную json-строку
             Log.d(LOG_TAG, strJson);
+            myName = (TextView) findViewById(R.id.myName);
+            myXP = (TextView) findViewById(R.id.myXP);
+            myLevelView = (TextView) findViewById(R.id.myLevel);
+            myProgressBar = (ProgressBar) findViewById(R.id.myProgressBar);
 
-            JSONObject dataJsonObj = null;
-            String secondName = "";
+//            JSONObject dataJsonObj = null;
+//            String secondName = "";
 
             try {
-                dataJsonObj = new JSONObject(strJson);
-                JSONArray friends = dataJsonObj.getJSONArray("friends");
 
-                // 1. достаем инфо о втором друге - индекс 1
-                JSONObject secondFriend = friends.getJSONObject(1);
-                secondName = secondFriend.getString("name");
-                Log.d(LOG_TAG, "Второе имя: " + secondName);
+                JSONObject reader = new JSONObject(strJson);
+//                JSONObject sys  = reader.getJSONObject("name");
 
-                // 2. перебираем и выводим контакты каждого друга
-                for (int i = 0; i < friends.length(); i++) {
-                    JSONObject friend = friends.getJSONObject(i);
+                // User name:
+                int id = Integer.parseInt(reader.getString("id"));
+                Log.d(LOG_TAG, "ID " + id);
 
-                    JSONObject contacts = friend.getJSONObject("contacts");
+                String name = reader.getString("name");
+                Log.d(LOG_TAG, "Имя " + name);
 
-                    String phone = contacts.getString("mobile");
-                    String email = contacts.getString("email");
-                    String skype = contacts.getString("skype");
+                String surname = reader.getString("surname");
+                Log.d(LOG_TAG, "surname " + surname);
 
-                    Log.d(LOG_TAG, "phone: " + phone);
-                    Log.d(LOG_TAG, "email: " + email);
-                    Log.d(LOG_TAG, "skype: " + skype);
-                }
+                myName.setText(name + " " + surname);
+
+
+                int experience = Integer.parseInt(reader.getString("experience"));
+                Log.d(LOG_TAG, "XP " + experience);
+
+                int myLevel = get_level(experience);
+//                String myLevel = get_level(experience) + "";
+//                String myExperience = experience + "";
+                int next_xp = level_xp(myLevel + 1);
+                int prev_xp = level_xp(myLevel);
+                int percent = 100 * (experience - prev_xp) / (next_xp - prev_xp);
+                myProgressBar.setProgress(percent);
+                myXP.setText(experience + "");
+                myLevelView.setText(myLevel + "");
+
+                String role = reader.getString("role");
+                Log.d(LOG_TAG, "role " + role);
+
+                JSONArray quests = reader.getJSONArray("quests");
+//                for(JSONObject quest in quests) {
+//                }
+                JSONObject quest = quests.getJSONObject(0);
+                // Quest name:
+                String q_name = quest.getString("name");
+                Log.d(LOG_TAG, "Имя " + q_name);
+
+
 
             } catch (JSONException e) {
                 e.printStackTrace();
